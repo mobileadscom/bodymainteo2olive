6819de511d25c7781fa835175936e47a78af9377
yklim <yk@mobileads.com>
2018-09-21 14:55:15 +0800

    twitter message correction

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -98,7 +98,7 @@ var app = {
 						user.saveLocal(user.info.id, response.data.couponCode, 'win', this.params.source); //rmb allow this back
 						this.localObj = user.getLocal();
 						this.initResult('win', couponLink);
-						var message = 'サラダスムージークーポンが当たりました! ' + encodeURI(couponLink);
+						var message = 'ボディメンテドリンククーポンが当たりました!  ' + encodeURI(couponLink);
 						if (user.info.id.indexOf('@') > -1) { // login via email
 				        	var emailContent = '<head><meta charset="utf-8"></head><div style="text-align:center;font-weight:600;color:#FF4244;font-size:28px;">おめでとうございます</div><br><br><div style="text-align:center;font-weight:600;">クーポンが当たりました！</div><a href="' + couponLink + '" target="_blank" style="text-decoration:none;"><button style="display:block;margin:20px auto;margin-bottom:40px;border-radius:5px;background-color:#E54C3C;border:none;color:white;width:200px;height:50px;font-weight:600;">クーポンを受取る</button></a>';
 				        	 user.sendEmail(user.info.id, 'Ienomistyle クーポンキャンペーン', emailContent);
